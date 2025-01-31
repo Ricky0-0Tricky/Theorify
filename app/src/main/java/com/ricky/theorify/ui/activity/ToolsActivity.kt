536105d0fcae4f1f48ac39707add463371d05d54
Joot111 <joaoamoroso04@gmail.com>
2025-01-31 17:10:12 +0000

    Design Tabela Ferramentas

---
+++ app/src/main/java/com/ricky/theorify/ui/activity/ToolsActivity.kt
@@ -0,0 +1,12 @@
+package com.ricky.theorify.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ricky.theorify.R
+
+class ToolsActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tools_page)
+    }
+}
